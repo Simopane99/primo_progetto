302311815d2cc94815e2e4fbad1eee6156df5f37
Simopane99 <simonepanichi7@gmail.com>
2025-01-08 11:51:32 +0100

    Passo 18(metà)

--- news/urls.py
+++ news/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home, articoloDetailView, index_news, listaArticoli
+from .views import home, articoloDetailView, index_news, listaArticoli, queryBase
 
 app_name='news'
 
@@ -9,5 +9,6 @@ urlpatterns= [
     path("lista_articoli/", listaArticoli, name="lista_articoli_giornalista"),
     path("lista_articoli/<int:pk>", listaArticoli, name="lista_articoli_giornalista"),
     path("articoli/<int:pk>", articoloDetailView, name="articolo_detail"),
+    path("query", queryBase, name="query_base"),
     path("", index_news, name="index_news")
 ]
